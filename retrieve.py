a8012c3c8d2aa0cf779fcda40dd8af73b0ffbd53
Kyunghyun Cho <kyunghyun.cho@nyu.com>
2019-01-02 12:56:04 +0900

    latest

--- retrieve.py
+++ retrieve.py
@@ -5,23 +5,37 @@ import sys
 import argparse
 
 import requests
+from requests.auth import HTTPBasicAuth
+
 import browsercookie
 
 def main(args):
-    cookie = browsercookie.chrome()
+    cookie = None
+    if args.cookie_from.strip() != "":
+        cookie = eval("browsercookie.{}()".format(args.cookie_from))
 
     url_file = open(args.file, 'r')
 
     for url in url_file:
+        url = url.strip()
         if url.strip() == "":
             continue
         print(url)
-        r = requests.get(url, cookies=cookie, verify=False)
-        with open(args.saveto+"/"+url.split("/")[-1].strip(), 'wb') as f:
-            f.write(r.content)
+        auth = None
+        if args.user != "":
+            auth = HTTPBasicAuth(args.user, args.password)
+        r = requests.get(url, cookies=cookie, verify=False, auth=auth)
+        if r.status_code != 200:
+            print('failed with {}'.format(r.status_code))
+        else:
+            with open(args.saveto+"/"+url.split("/")[-1].strip(), 'wb') as f:
+                f.write(r.content)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+    parser.add_argument('-user', default="", type=str)
+    parser.add_argument('-password', default="", type=str)
+    parser.add_argument("-cookie-from", default="", type=str)
     parser.add_argument('file', type=str)
     parser.add_argument('saveto', type=str)
 
